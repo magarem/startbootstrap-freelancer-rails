e21276c8fa81b28c80d14db6c18430550842fb41
marcelo <contato@magaweb.com.br>
2016-06-06 14:40:18 -0300

    email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+   config.action_mailer.smtp_settings = { enable_starttls_auto: false  }
+  #config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
 end
